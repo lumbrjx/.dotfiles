d5f95bbd5f49461d41ebb8030e3d087eabaeeb71
lumbrjx <kaibitayeb01@gmail.com>
2025-06-26 17:04:42 +0100

    Update: improve nvim

---
+++ .config/nvim/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+local d ={
+{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000, -- Load before everything else
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- or latte, frappe, macchiato
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+}
+return {}
